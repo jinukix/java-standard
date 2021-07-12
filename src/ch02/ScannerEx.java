addf373f86f6e13f4ac381bb537a6b7daff871d1
Jinuk <wfo3977@gmail.com>
2021-07-12 21:59:38 +0900

    ch01

---
+++ src/ch02/ScannerEx.java
@@ -0,0 +1,17 @@
+package ch02;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("정수를 입력해 주세요.>");
+
+        String input = scanner.nextLine();
+        int num = Integer.parseInt(input);
+
+        System.out.println("입력내용: " + input);
+        System.out.printf("num=%d\n", num);
+    }
+}
